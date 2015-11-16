2ba47ba8d9347c12fe5e594db812a04bcab44dfd
Jingming Chen <chenjm8683@gmail.com>
2015-11-15 16:08:48 -0800

    finished 03a bonus

--- lib/03a_nobel_bonus.rb
+++ lib/03a_nobel_bonus.rb
@@ -14,5 +14,21 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT DISTINCT
+      n1.yr
+    FROM
+      (SELECT
+        yr, subject
+      FROM
+        nobels
+      WHERE subject = 'Physics') AS n1
+    LEFT OUTER JOIN
+      (SELECT
+        yr, subject
+      FROM nobels
+      WHERE subject = 'Chemistry') AS n2
+    ON n1.yr = n2.yr
+    WHERE n1.subject = 'Physics'
+      AND n2.subject IS NULL
   SQL
 end
